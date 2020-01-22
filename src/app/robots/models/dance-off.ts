e436444224ec17090a49d54acafdc91a40949d3f
Lukas Skofitsch <i.combat.the.wombat@gmail.com>
2020-01-22 09:33:15 +0100

    added robot and danceoff models

---
+++ src/app/robots/models/dance-off.ts
@@ -0,0 +1,9 @@
+/**
+ * DanceOff model as specified by the [documentation](https://challenge.parkside-interactive.com/docs/)
+ */
+export interface DanceOff {
+  id: number;
+  winner: number;
+  loser: number;
+  dancedAt: string;
+}
---
+++ src/app/robots/models/robot.ts
@@ -0,0 +1,10 @@
+/**
+ * Robot model as specified by the [documentation](https://challenge.parkside-interactive.com/docs/)
+ */
+export interface Robot {
+  id: number;
+  name: string;
+  experience: number;
+  outOfOrder: boolean;
+  avatar: string;
+}
